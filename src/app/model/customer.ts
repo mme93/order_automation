70ea58f2f504a45866c1866ea818b253843d7f49
magantus@googlemail.com <Extrem93!>
2022-08-02 14:19:11 +0200

    init customer model

---
+++ src/app/model/customer.ts
@@ -0,0 +1,10 @@
+export interface Customer {
+  firstName: string;
+  lastName: string;
+  email: string;
+  street: string;
+  town: string;
+  postcode: number;
+  callNumber: string;
+  info: string;
+}
