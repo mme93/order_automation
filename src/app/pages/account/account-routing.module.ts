40909bae888db634e85a0dbce6722d5f40b5f8a1
magantus@googlemail.com <Extrem93!>
2022-08-02 12:13:58 +0200

    define routes

--- src/app/pages/account/account-routing.module.ts
+++ src/app/pages/account/account-routing.module.ts
@@ -2,11 +2,32 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AccountPage } from './account.page';
+import {RegistrationComponent} from './registration/registration.component';
+import {LoginComponent} from './login/login.component';
+import {PasswordResetComponent} from './password-reset/password-reset.component';
+import {ProfileComponent} from './profile/profile.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AccountPage
+  },
+  {
+    path: 'registry',
+    component: RegistrationComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'reset',
+    component: PasswordResetComponent
+  }
+  ,
+  {
+    path: 'profile',
+    component: ProfileComponent
   }
 ];
 
