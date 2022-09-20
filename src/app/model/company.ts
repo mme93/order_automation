ea346622ab154d5e0eadc5aa485fc6f220cba0f8
magantus@googlemail.com <Extrem93!>
2022-09-21 01:15:04 +0200

    update

---
+++ src/app/model/company.ts
@@ -0,0 +1,11 @@
+export interface Company{
+  companyName: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  city: string;
+  street: string;
+  postalCode: string;
+  callNumber: string;
+  sector: string;
+}
