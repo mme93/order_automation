a7331e3f1c62ad38009e8aaeba90d9cee9d62d75
magantus@googlemail.com <Extrem93!>
2022-10-09 01:57:30 +0200

    update

--- src/app/pages/account/login/login.component.ts
+++ src/app/pages/account/login/login.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {LoginService} from '../../../services/http/account/login.service';
 import {Router} from '@angular/router';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,16 +12,25 @@ export class LoginComponent {
   username = 'admin';
   password = 'admin';
   isLoading = false;
+  isLogin = true;
 
   constructor(
     private loginService: LoginService,
-    private route: Router
+    private route: Router,
+    public formBuilder: FormBuilder
   ) {
   }
 
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  ionicForm = this.formBuilder.group({
+    name: ['dhall', [Validators.required, Validators.minLength(1)]],
+    password: ['admin', [Validators.required, Validators.minLength(1)]]
+  });
+
   login() {
     this.isLoading = true;
-    this.loginService.login(this.username, this.password).subscribe(
+
+    this.loginService.login(this.ionicForm.controls.name.value, this.ionicForm.controls.password.value).subscribe(
       response => {
         this.isLoading = false;
         localStorage.setItem('token', response.token);
@@ -29,9 +39,9 @@ export class LoginComponent {
         this.route.navigate(['home']);
       }, error => {
         this.isLoading = false;
+        this.isLogin = false;
         console.log(error);
       }
     );
   }
-
 }
