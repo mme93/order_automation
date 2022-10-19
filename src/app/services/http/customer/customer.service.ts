75de872724a15801f388f72785034006752ea474
magantus@googlemail.com <Extrem93!>
2022-10-19 21:51:19 +0200

    update

--- src/app/services/http/customer/customer.service.ts
+++ src/app/services/http/customer/customer.service.ts
@@ -4,17 +4,6 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../../../environments/environment';
 import {Observable} from 'rxjs';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    'Content-Type': 'application/json',
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    Authorization: 'Bearer ' + localStorage.getItem('token'),
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    Company: localStorage.getItem('company')
-  })
-};
-
 @Injectable({
   providedIn: 'root'
 })
@@ -24,14 +13,44 @@ export class CustomerService {
   }
 
   updateCustomer(customer: Customer) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        'Content-Type': 'application/json',
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Company: localStorage.getItem('company')
+      })
+    };
     return this.http.put(environment.backendUrl + 'customer/update', JSON.stringify(customer), httpOptions);
   }
 
   loadAllCustomer(): Observable<Customer[]> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        'Content-Type': 'application/json',
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Company: localStorage.getItem('company')
+      })
+    };
     return this.http.get<Customer[]>(environment.backendUrl + 'customer/all', httpOptions);
   }
 
   createCustomer(customer: Customer) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        'Content-Type': 'application/json',
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Company: localStorage.getItem('company')
+      })
+    };
     return this.http.post<Customer[]>(environment.backendUrl + 'customer/create', JSON.stringify(customer), httpOptions).subscribe(
       response => console.log(response),
       error => console.log(error)
@@ -39,11 +58,31 @@ export class CustomerService {
   }
 
   deleteCustomer(id: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        'Content-Type': 'application/json',
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Company: localStorage.getItem('company')
+      })
+    };
     return this.http.delete<Customer[]>(environment.backendUrl +'customer/delete/' + id, httpOptions);
   }
 
 
   findCustomerByID(customerID: any): Observable<Customer> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        'Content-Type': 'application/json',
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Company: localStorage.getItem('company')
+      })
+    };
     return this.http.get<Customer>(environment.backendUrl + 'customer/'+customerID, httpOptions);
   }
 }
