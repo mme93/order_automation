a6b7791b94ae3c3dc696b6e2794c787b3eb5b9f2
magantus@googlemail.com <Extrem93!>
2023-01-13 00:58:18 +0100

    update

--- src/app/pages/vehicle/registrationdocument/registrationdocument.module.ts
+++ src/app/pages/vehicle/registrationdocument/registrationdocument.module.ts
@@ -7,13 +7,27 @@ import { IonicModule } from '@ionic/angular';
 import { RegistrationdocumentPageRoutingModule } from './registrationdocument-routing.module';
 
 import { RegistrationdocumentPage } from './registrationdocument.page';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RegistrationdocumentPageRoutingModule
+    RegistrationdocumentPageRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule
   ],
   declarations: [RegistrationdocumentPage]
 })
